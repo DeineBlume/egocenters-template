545e5991abf1fb5d78dc0f4250c0110bf72e8054
noriginalno <78056210+noriginalno@users.noreply.github.com>
2023-10-20 09:17:34 +0300

    Update egocenters.py
    brushed code, change degree_centrality to betweenness_centrality

--- egocenters.py
+++ egocenters.py
@@ -3,6 +3,32 @@ import networkx as nx
 def find_impostor(edgelist, pseudocenters):
     G = nx.Graph()
     G.add_edges_from(edgelist)
-    central_dict = nx.degree_centrality(G)
+    centralities = nx.betweenness_centrality(G)
+    centralitiy_of_inputs = {a: centralities[a] for a in pseudocenters}
+    return sorted(centralitiy_of_inputs.items(), key=lambda x: x[1])[0][0]
+
+# in this execution i have chosen the best centrality
+if __name__ == "__main__":
+
+    centers = [0, 107, 1684, 1912, 3437, 348, 3980, 414, 686, 698]
+    pseudocenters = [0, 107, 1684, 1912, 3437, 348, 612, 3980, 414, 686, 698]
+
+    real_impostor = 612
+
+    f = open('facebook_combined.txt')
+    lines = f.readlines()
+
+    def spl2(x):
+        y = x.split()
+        return (int(y[0]), int(y[1]))
+
+    edges = list(map(spl2, lines))
+
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    # this row was variated via centrality method
+    central_dict = nx.betweenness_centrality(G)
     check = {a: central_dict[a] for a in pseudocenters}
-    return sorted(check.items(), key=lambda x: x[1])[0][0]
+    print('ours', *sorted(sorted(check.items(), key=lambda x: x[1])[:10]), sep="\n")
+    print('theirs', sorted(centers))
